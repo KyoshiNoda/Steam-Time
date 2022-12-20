74be3f90882a30097f3a84e88087f33d9507b8d5
Dylan Nandlall <dan6705@rit.edu>
2022-12-20 18:33:26 -0500

    i forgot what i changed

--- backend/venv/server/steam_requests_test.py
+++ backend/venv/server/steam_requests_test.py
@@ -1,7 +1,12 @@
 import requests
+import json
 
 api_key = "69165F7C2940B1D23B6A67783A944BAB"
 steamid = "76561198871279330"
 
 response = requests.get(f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steamid}&format=json')
-print(response.text)
+response_dict = json.loads(response.text)
+print(response_dict)
+
+
+# print(response.text)
