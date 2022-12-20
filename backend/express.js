231003b6482cb8ae768b2b6d11d76cfc2ad673dd
KyoshiNoda <kyoshisew@gmail.com>
2022-12-20 11:49:38 -0500

    added express to test steam API

--- backend/express.js
+++ backend/express.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', "ejs");
 
 app.listen(3001,()=>{
   console.log("Listening to port 3001");
 });
 
 app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.render("view");
 });
