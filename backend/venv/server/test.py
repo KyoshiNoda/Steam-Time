02d20b8bb3685a0fc62ce5fa2050d4c22ba55cad
Dylan Nandlall <dan6705@rit.edu>
2022-12-20 16:50:19 -0500

    steam_request.py queried api.steampowered.com for games played info

--- backend/venv/server/test.py
+++ backend/venv/server/test.py
@@ -14,5 +14,5 @@ print("Logged on as:", client.user.name)
 print("Community profile:", client.steam_id.community_url)
 print("Last logon:", client.user.last_logon)
 print("Last logoff:", client.user.last_logoff)
-
+print("Current Games Played", client.user.get_ps('game_played_app_id'))
 client.logout()
