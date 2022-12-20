50b80b2fd9ca9c0cd0600c5ab5bfd0f37f714608
Dylan Nandlall <dan6705@rit.edu>
2022-12-20 15:32:58 -0500

    added python virtual environment

---
+++ backend/venv/server/server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+import sys
+
+from _thread import *
+
+
+lock = threading.Lock()
+
+def server_listener(client):
+    while True:
+        data = client.recv(1024)
+        if not data:
+            print('Bye')
+            lock.release()
+            break
+        client.send(data)
+    client.close()
+
+def main():
+    host = ""
+    port = 12345
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind((host, port))
+    except socket.error as message:
+        print('Bind failed. Error Code: {} Message {}',str(message[0]), str(message[1]))
+        sys.exit()
+    print("socket binded to {}", port)
+
+    s.listen(5)
+    print("socket is listening")
+
+    while True:
+        client, addr = s.accept()
+        lock.acquire()
+        print("Connected to :{} :{}", addr[0], addr[1])
+
+        start_new_thread(server_listener, (client))
+    s.close()
+
+if __name__ == '__main__':
+    main()
---
+++ backend/venv/server/test.py
@@ -0,0 +1,18 @@
+from steam.client import SteamClient
+from steam.enums import EResult
+
+client = SteamClient()
+
+result = client.cli_login()
+
+if result != EResult.OK:
+    print("Failed to login: %s" % repr(result))
+    raise SystemExit
+
+print("-"*20)
+print("Logged on as:", client.user.name)
+print("Community profile:", client.steam_id.community_url)
+print("Last logon:", client.user.last_logon)
+print("Last logoff:", client.user.last_logoff)
+
+client.logout()
