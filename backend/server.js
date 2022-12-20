95aad4f4edda55083d15c06231a0e78b64c04d3c
KyoshiNoda <kyoshisew@gmail.com>
2022-12-20 11:18:36 -0500

    testing steam api

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('test');
+const button = document.querySelector('button');
+const fetchAPI = () =>{
+  const url = `http://api.steampowered.com/ISteamApps/GetAppList/v2`;
+  fetch(url).then((res)=>{
+    res.json()
+  }).then((data)=>{
+    console.log(data);
+  })
+}
+
+button.addEventListener('click',()=>{
+  fetchAPI();
+});
