a2aa55c92812e5152f022aa4999c351c363867e6
unknown <kyoshisew@gmail.com>
2022-12-20 18:46:57 -0500

    changed title for navbar with reroute to homepage

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <div className="navbar bg-base-100">
         <div className="flex-1">
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <a href = '/'className="btn btn-ghost normal-case text-xl">SteamTime</a>
         </div>
         <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
