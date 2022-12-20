b5f85419124e388661516928102431c6cb0bd725
KyoshiNoda <kyoshisew@gmail.com>
2022-12-20 11:56:21 -0500

    installed cors for policy use

--- backend/express.js
+++ backend/express.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,  //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', "ejs");
