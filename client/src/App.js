9b323e876bf53b08b17c606a606e81c86d0f4d9e
unknown <kyoshisew@gmail.com>
2022-12-20 19:37:55 -0500

    removed junk components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Routes,Route} from 'react-router-dom';
 import Homepage from './pages/Homepage';
-import Account from './pages/Account';
 import Statistics from './pages/Statistics';
 import Timer from './pages/Timer';
 import Signout from './pages/Signout';
@@ -11,7 +10,6 @@ function App() {
     <>
       <Routes>
         <Route path = '/' element = {<Homepage/>}/>
-        <Route path = '/account' element = {<Account/>}/>
         <Route path = '/Settings' element = {<Settings/>}/>
         <Route path = '/statistics' element = {<Statistics/>}/>
         <Route path = '/timer' element = {<Timer/>}/>
