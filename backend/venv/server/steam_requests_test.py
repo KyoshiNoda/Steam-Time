d0268e5a233eb90b366d2f5cc24f471c355cba28
Dylan Nandlall <dan6705@rit.edu>
2022-12-20 20:45:43 -0500

    steam_query.py added helper functions to get data from web api

--- backend/venv/server/steam_requests_test.py
+++ backend/venv/server/steam_requests_test.py
@@ -2,17 +2,23 @@ import requests
 import json
 
 api_key = "69165F7C2940B1D23B6A67783A944BAB"
-steamid = "76561198871279330"
+steam_id = "76561198871279330"
 
 # response = requests.get(f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steamid}&format=json')
-# response_data = json.loads(response.text)
-# print(response_data['response']['games'][0])
-# print(response_data)
+# response_data = json.loads(requests.get(f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steamid}&format=json').text)
+# # print(response_data['response']['games'][0])
+# # print(response_data)
+# for i in range(len(response_data['response']['games'])):
+#     if response_data['response']['games'][i]['appid'] == 550:
+#         print(response_data['response']['games'][i]['playtime_forever'])
 
-app_list = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2")
-app_list_data = json.loads(app_list.text)
-for x in app_list_data['applist']['apps']:
-    if x == 41:
-        print(app_list_data['applist']['apps'][x])
-        break
-# print(app_list_data['applist']['apps'][41])
+# app_list = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2")
+# app_list_data = json.loads(requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2").text)
+# for i in range(len(app_list_data['applist']['apps'])):
+#     if app_list_data['applist']['apps'][i]['appid'] == 1648300:
+#         print(app_list_data['applist']['apps'][i]['name'])
+#         break
+# print(app_list_data['applist']['apps'][0]['appid'])
+
+result = json.loads(requests.get(f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_id}&format=json').text)
+print(result)
