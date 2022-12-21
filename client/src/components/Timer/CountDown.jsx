10a7543804a71fc43024c64015597ad7d6752d11
KyoshiNoda <kyoshisew@gmail.com>
2022-12-21 03:19:16 -0500

    added countdown implementation

---
+++ client/src/components/Timer/CountDown.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function CountDown() {
+  return (
+    <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span className="countdown font-mono text-5xl">
+          <span style={{ "--value": 10 }}></span>
+        </span>
+        hours
+      </div>
+      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span className="countdown font-mono text-5xl">
+          <span style={{ "--value": 24 }}></span>
+        </span>
+        min
+      </div>
+      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span className="countdown font-mono text-5xl">
+          <span style={{ "--value": 58 }}></span>
+        </span>
+        sec
+      </div>
+    </div>
+  );
+}
+
+export default CountDown;
--- client/src/pages/Timer.jsx
+++ client/src/pages/Timer.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Background from '../components/Background'
-import NavBar from '../components/NavBar/NavBar'
+import React from "react";
+import Background from "../components/Background";
+import NavBar from "../components/NavBar/NavBar";
+import CountDown from "../components/Timer/CountDown";
 function Timer() {
   return (
     <Background>
-      <NavBar/>
-      Timer
+      <NavBar />
+      <div className="flex justify-center items-center h-96 bg-base-300">
+        <CountDown />
+      </div>
     </Background>
-  )
+  );
 }
 
-export default Timer
+export default Timer;
