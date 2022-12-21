5f70f07aae7290acfb4fd4634990c65703098af2
KyoshiNoda <kyoshisew@gmail.com>
2022-12-21 03:40:46 -0500

    added useEffect with pending countdown

--- client/src/components/Timer/CountDown.jsx
+++ client/src/components/Timer/CountDown.jsx
@@ -1,27 +1,10 @@
 import React from "react";
 
-function CountDown() {
+function CountDown(props) {
   return (
-    <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-        <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": 10 }}></span>
-        </span>
-        hours
-      </div>
-      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-        <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": 24 }}></span>
-        </span>
-        min
-      </div>
-      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-        <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": 58 }}></span>
-        </span>
-        sec
-      </div>
-    </div>
+    <span className="countdown font-mono text-5xl">
+      <span style={props.value}></span>
+    </span>
   );
 }
 
---
+++ client/src/components/Timer/Time.jsx
@@ -0,0 +1,38 @@
+import React,{useState,useEffect} from "react";
+import Countdown from 
+function Time() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setValue((v) => (v <= 0 ? value : v - 1));
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value]);
+
+  return (
+    <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <Countdown className="font-mono text-5xl" value={15} />
+        days
+      </div>
+      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <Countdown className="font-mono text-5xl" value={10} />
+        hours
+      </div>
+      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <Countdown className="font-mono text-5xl" value={24} />
+        min
+      </div>
+      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <Countdown className="font-mono text-5xl" value={value} />
+        sec
+      </div>
+    </div>
+  );
+}
+
+export default Time;
