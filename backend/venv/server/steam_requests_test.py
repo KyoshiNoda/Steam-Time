82ec2bddf1c6c000776603e7df07e20e52287a68
Dylan Nandlall <dan6705@rit.edu>
2022-12-20 19:52:27 -0500

    steam request applist test

--- backend/venv/server/steam_requests_test.py
+++ backend/venv/server/steam_requests_test.py
@@ -4,9 +4,15 @@ import json
 api_key = "69165F7C2940B1D23B6A67783A944BAB"
 steamid = "76561198871279330"
 
-response = requests.get(f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steamid}&format=json')
-response_dict = json.loads(response.text)
-print(response_dict)
+# response = requests.get(f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steamid}&format=json')
+# response_data = json.loads(response.text)
+# print(response_data['response']['games'][0])
+# print(response_data)
 
-
-# print(response.text)
+app_list = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2")
+app_list_data = json.loads(app_list.text)
+# for x in app_list_data['applist']['apps']:
+#     if x == 41:
+#         print(app_list_data['applist']['apps'][x])
+#         break
+print(app_list_data['applist']['apps'][41])
