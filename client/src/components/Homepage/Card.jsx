0b95cee88c7d47c43c747cb4150534d3b10363a5
unknown <kyoshisew@gmail.com>
2022-12-22 18:23:23 -0500

    pulled data from getOwnedGames into Card component

--- client/src/components/Homepage/Card.jsx
+++ client/src/components/Homepage/Card.jsx
@@ -1,26 +1,36 @@
 import React, {useState,useEffect} from 'react'
 import Axios from 'axios';
 function Card() {
+  let games = {}
+  const [gameList,setGameList] = useState(games);
 
   useEffect(()=>{
-    Axios.get('http://localhost:6969/profile')
+    Axios.get('http://localhost:6969/ownedgames')
     .then(res =>{
-      console.log(res);
+      games = res.data.response.games;
+      setGameList(games);
     })
     .catch(err =>{
       console.log(err);
     })
-  })
-
-
-
-
-
+  },[]);
+  console.log(gameList);
+  let totalMins = 0;
+  let maxTime = 0;
+  let favGame = '';
+  for(let i = 0; i < gameList.length; i++){
+    totalMins += gameList[i].playtime_forever;
+    if(gameList[i].playtime_forever > maxTime){
+      favGame = gameList[i].appid;
+      maxTime = gameList[i].playtime_forever;
+    }
+  }
+  let totalHours = Math.round(totalMins / 60);
   const user = {
-      name : 'PlentyJapan',
-      totalGames: 79,
-      totalPlayTime: 1230,
-      favGame: 'Watch_Dogs 2',
+      name : 'Dilan',
+      totalGames: gameList.length,
+      totalPlayTime: totalHours,
+      favGame: favGame,
       image: 'https://avatars.akamai.steamstatic.com/b85bfe3287228fc57f8f555dbaa71da329abf8f9_full.jpg'
   };
   return (
@@ -34,16 +44,6 @@ function Card() {
             <h2>Total Playtime: {user.totalPlayTime} hours</h2>
             <h2>Favorite Game: {user.favGame}</h2>
         </div>
-      </div>
-
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
-          <figure><img className = 'w-full' src= {user.image} alt = 'steamLogo'/></figure>
-          <div className="card-body p-5 m-5 font-semibold">
-              <h1 className="card-title justify-center text-3xl">{user.name}</h1>
-              <h2>Games Owned: {user.totalGames}</h2>
-              <h2>Total Playtime: {user.totalPlayTime} hours</h2>
-              <h2>Favorite Game: {user.favGame}</h2>
-          </div>
       </div>
     </div>
 
