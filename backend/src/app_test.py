db57899d38c777461933d5ab8d51a25a7eafc165
Dylan Nandlall <dan6705@rit.edu>
2022-12-23 15:00:01 -0500

    added get app image and get steamid to flask server, made a basic app runtime counter w/ steam web api, changed parameters for steam_webapi_query class

--- backend/src/app_test.py
+++ backend/src/app_test.py
@@ -1,17 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 import steam_query_class
 
-query_object = steam_query_class.Steam_WebAPI_Query(76561198871279330)
+query_object = steam_query_class.Steam_WebAPI_Query("Dilian1")
 
 
 @app.route("/profile", methods=['GET'])
 def profile():
     return query_object.get_profile()
 
-@app.route("/ownedgames", methods=['GET'])
-def ownedgames():
+@app.route("/owned-games", methods=['GET'])
+def owned_games():
     return query_object.get_owned_games()
+
+@app.route("/get-app-image", method=['POST', 'GET'])
+def app_image():
+    if request.method == 'POST':
+        appid = request.form['appid']
+    return query_object.get_app_img_url(appid)
+
+@app.route("/get_steamid", method=['GET'])
+def name_to_steamid():
+    return query_object.steam_id
+
+
+# @app.route("/login", method=['POST', 'GET'])
+# def login():
+#     if request.method == 'POST':
+#         user = request.form['name']
+
 
 
 if __name__ == "__main__":
