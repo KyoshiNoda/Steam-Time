eebd0a0ceff3e7f611b8a495405cb69e6e97bf90
Dylan Nandlall <dan6705@rit.edu>
2022-12-23 13:46:12 -0500

    app_runtime counter created

--- backend/src/app_runtime.py
+++ backend/src/app_runtime.py
@@ -1,35 +1,54 @@
 import requests
 import json
 import time
+import threading
+
+lock = threading.Lock()
 
 steam_id = 76561198871279330
 api_key = "69165F7C2940B1D23B6A67783A944BAB"
 
-game_tracker = {}
+time_counter = 0
 
 def game_poller():
-    global game_tracker
+    response_data = json.loads(requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=69165F7C2940B1D23B6A67783A944BAB&steamids=76561198871279330").text)
+    if 'gameextrainfo' in response_data['response']['players'][0]:
+        return True
+    return False
 
+def counter():
+    global time_counter
+    start = time.time()
     while True:
-        response_data = json.loads(requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=69165F7C2940B1D23B6A67783A944BAB&steamids=76561198871279330").text)
-        if 'gameextrainfo' in response_data['response']['players'][0]:
-            game_name = response_data['response']['players'][0]['gameextrainfo']
-            if game_name not in game_tracker:
-                game_tracker[game_name] = {"runtime": 0, "gameid": response_data['response']['players'][0]['gameid']}
-            else:
-                game_tracker[game_name]["runtime"] += 1
-        else:
-            print("Sleep 10 seconds")
-            time.sleep(10)
-            print("Wake up")
+        if game_poller() == True:
+            lock.acquire()
+            time_counter = int(time.time() - start)
+            lock.release()
+            time.sleep(1)
             continue
+        else:
+            break
+
+def printer():
+    global time_counter
+    while True:
+        lock.acquire()
+        print(time_counter)
+        lock.release()
         time.sleep(1)
-        print(game_tracker)
-
-
 
 def main():
-    game_poller()
+    t1 = threading.Thread(target=counter)
+    t2 = threading.Thread(target=printer)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+
+
 
 if __name__ == '__main__':
     main()
