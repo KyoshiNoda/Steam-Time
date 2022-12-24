3ef668cdcc8add576bab187214eae5738defec85
unknown <kyoshisew@gmail.com>
2022-12-24 00:51:12 -0500

    added functionality with proper components

--- client/src/components/Timer/TimeForm.jsx
+++ client/src/components/Timer/TimeForm.jsx
@@ -1,53 +1,51 @@
 import React, {useState} from "react";
 
-function TimeForm() {
+function TimeForm({getTime}) {
   const [hours,setHours] = useState();
   const [mins, setMins] = useState();
 
-  const hoursHandler = (event)=>{
-    setHours(event.target.value);
-    console.log(hours);
-  }
+  const formHandler = (event) =>{
+    event.preventDefault();
+    getTime(hours,mins);
+  };
 
-  const minsHandler = (event) =>{
-    setMins(event.target.value);
-    console.log(mins);
-  }
-
+  const hoursHandler = (event)=>{ setHours(event.target.value) }
+  const minsHandler = (event) =>{ setMins(event.target.value) }
 
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-center gap-10">
-        <div className="form-control w-32 max-w-xs">
-          <label className="label">
-            <span className="label-text font-semibold">Hours</span>
-          </label>
-          <input
-            type="number"
-            onChange={hoursHandler}
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
+    <form onSubmit={formHandler}>
+        <div className="flex flex-col">
+            <div className="flex justify-center gap-10">
+                <div className="form-control w-32 max-w-xs">
+                    <label className="label">
+                        <span className="label-text font-semibold">Hours</span>
+                    </label>
+                    <input
+                    type="number"
+                    onChange={hoursHandler}
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                    />
+                </div>
 
-        <div className="form-control w-32 max-w-xs">
-          <label className="label">
-            <span className="label-text font-semibold">Minutes</span>
-          </label>
-          <input
-            type="number"
-            onChange={minsHandler}
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
-      </div>
+                <div className="form-control w-32 max-w-xs">
+                    <label className="label">
+                        <span className="label-text font-semibold">Minutes</span>
+                    </label>
+                    <input
+                    type="number"
+                    onChange={minsHandler}
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                    />
+                </div>
+            </div>
 
-      <div className="flex justify-center m-3">
-        <button className="btn btn-primary w-32">Submit Me</button>
-      </div>
-    </div>
+            <div className="flex justify-center m-3">
+                <button type = 'submit' className="btn btn-primary w-32">Submit Me</button>
+            </div>
+        </div>
+    </form>
   );
 }
-
 export default TimeForm;
--- client/src/pages/Timer.jsx
+++ client/src/pages/Timer.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import NavBar from '../components/NavBar/NavBar';
 import CountDownTimer from "../components/Timer/CountDownTimer";
 import TimeForm from "../components/Timer/TimeForm";
 function Timer() {
+  const [finalTime,setFinalTime] = useState(0);
+  const timeHandler = (uHours,uMins) =>{
+    const timeMil =(uHours*60*60+uMins*60)*1000;
+    setFinalTime(timeMil);
+    console.log(finalTime); 
+  };
   return (
     <>
-      <NavBar />
-      <CountDownTimer timeMS = {401410104014010}/>
-      <TimeForm/>
+      <NavBar/>
+      <CountDownTimer timeMS = {finalTime}/>
+      <TimeForm getTime = {timeHandler}/>
     </>
   );
 }
