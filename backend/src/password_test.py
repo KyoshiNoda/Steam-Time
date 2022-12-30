3cbef93b64ecfd3b69133a8fed6a6d17f6bb4d0d
Dylan Nandlall <dan6705@rit.edu>
2022-12-29 21:25:58 -0500

    removed app_runtime.py and password_test.py from working repo

--- backend/src/password_test.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-password = u"wowzer123"
-hashed_password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-
-string_password = hashed_password.decode('utf8')
-
-password = u"wowzer123"
-print(bcrypt.checkpw(password.encode('utf8'), string_password.encode('utf8')))
