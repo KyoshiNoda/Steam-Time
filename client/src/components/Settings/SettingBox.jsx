7d3ce2c34b241c7d41a3880c1c76b01e78472c4c
KyoshiNoda <kyoshisew@gmail.com>
2023-01-03 13:47:34 -0500

    added layer components for settings page

---
+++ client/src/components/Settings/SettingBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SettingForm from './SettingForm';
+
+function SettingBox() {
+  return (
+    <div className="bg-slate-100 w-1/2 justify-evenly font-bold p-10 rounded space-y-5">
+      <h1 className="text-3xl text-black">John Doe</h1>
+      <SettingForm/>
+    </div>
+  );
+}
+
+export default SettingBox;
---
+++ client/src/components/Settings/SettingForm.jsx
@@ -0,0 +1,118 @@
+import React,{useState} from 'react';
+
+function SettingForm() {
+
+  const [firstName,setFirstName] = useState();
+  const [lastName,setLastName] = useState();
+  const [email,setEmail] = useState();
+  const [apiKey,setApiKey] = useState();
+  const [username,setUsername] = useState("Dlyan");
+
+  const firstNameHandler = (event) =>{
+    setFirstName(event.target.value);
+    console.log(firstName);
+  };
+
+  const lastNameHandler = (event) =>{
+    setLastName(event.target.value);
+    console.log(lastName);
+  };
+
+  const emailHandler = (event) =>{
+    setEmail(event.target.value);
+    console.log(email);
+  };
+
+  const apiKeyHandler  = (event) =>{
+    setApiKey(event.target.value);
+    console.log(apiKey);
+  }
+  const userNameHandler = (event) =>{
+    setUsername(event.target.value);
+    console.log(username);
+  }
+
+  return (
+    <form>
+      <div className="flex m-10">
+        <div className="flex-1 first:mr-10">          
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text lg:text-xl">User Name</span>
+            </label>
+            <input
+              onChange={userNameHandler}
+              type="text"
+              placeholder="Dilan"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text lg:text-xl">First Name</span>
+            </label>
+            <input
+              onChange={firstNameHandler}
+              type="text"
+              placeholder="John"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text lg:text-xl">Last Name</span>
+            </label>
+            <input
+              onChange={lastNameHandler}
+              type="text"
+              placeholder="Doe"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+        </div>
+
+        <div className="mr-10">
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text lg:text-xl">Steam URL</span>
+            </label>
+            <input
+              onChange = {emailHandler}
+              type="text"
+              placeholder="https://steamcommunity.com/id/Dilian1"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text lg:text-xl">API Key</span>
+            </label>
+            <input
+              onChange={apiKeyHandler}
+              type="text"
+              placeholder="JH2H32K3214H1213"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text lg:text-xl">Email</span>
+            </label>
+            <input
+              onChange={apiKeyHandler}
+              type="text"
+              placeholder="JohnDoe@gmail.com"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center gap-5">
+        <button className="btn btn-outline btn-success">Save Changes</button>
+        <button className="btn btn-outline btn-warning">Change Password</button>
+      </div>
+    </form>
+  );
+}
+
+export default SettingForm;
