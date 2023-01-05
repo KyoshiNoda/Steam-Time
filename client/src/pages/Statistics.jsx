3703cb62156e7204450180dcff81c82909de166f
unknown <kyoshisew@gmail.com>
2023-01-04 20:38:14 -0500

    added bar chart

--- client/src/pages/Statistics.jsx
+++ client/src/pages/Statistics.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-
+import React,{useState} from 'react';
 import NavBar from '../components/NavBar/NavBar';
+import { UserData } from '../Utils/Data';
+import BarChart from '../components/Statistics/BarChart';
 function Statistics() {
+  const [userData,setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets : [{
+      label : 'Users Gained',
+      backgroundColor: [
+        "rgba(75,192,192,1)",
+        "#ecf0f1",
+        "#50AF95",
+        "#f3ba2f",
+        "#2a71d0",
+      ],
+      data: UserData.map((data) => data.userGain),
+    }]
+  });
   return (
     <>
       <NavBar />
-      Statistics
+      <div className='flex justify-center'>
+        <div className='w-1/2 p-5'>
+          <BarChart chartData={userData}/>
+        </div>
+      </div>
+
     </>
   );
 }
