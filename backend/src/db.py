acf80069a27248bc62b5874a40bcb89e5135cbcc
Dylan Nandlall <dan6705@rit.edu>
2023-01-05 13:05:28 -0500

    DB create_account function

--- backend/src/db.py
+++ backend/src/db.py
@@ -8,23 +8,24 @@ def get_database(database_name):
     client = MongoClient(MONGO_DB_CONNECTION_STRING)
     return client[database_name]
 
-def create_account(email, api_key_hash, password_hash):
+def create_account(email_hash, steam_name_hash, api_key_hash, password_hash):
     database = get_database(MONGO_DB_DATABASE)
     collection = database[MONGO_DB_COLLECTION]
 
     item = {
-        "email" : email,
-        "api_key" : api_key_hash,
+        "email" : email_hash,
+        "steam_name" : steam_name_hash,
         "password" : password_hash,
+        "api_key" : api_key_hash,
     }
 
     collection.insert_one(item)
 
 def main():
-    ### Data Already Used, edit data and the run
+    ### Data Already Used, edit data and run
     
-    # create_account("test@example.com", "1", "examplepassword")
-    # create_account("test2@example.com", "2", "examplepassword2")
+    # create_account("test1@example.com", "example_name1", "examplepassword1", "1")
+    # create_account("test2@example.com", "example_name2", "examplepassword2", "2")
     pass
 
 if __name__ == "__main__":
