260bd7690d361f55c6b198161be53167f44f8d57
Dylan Nandlall <dan6705@rit.edu>
2024-01-06 17:17:56 -0500

    added ownedgames, recentlyplayedgames, and steamlevel api endpoints that query the steam api. Updated requirements.txt file with requests module

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from database.db import db
 from database.models import User
 from dotenv import load_dotenv
 from pysteamsignin.steamsignin import SteamSignIn
+import requests
 import os
 load_dotenv()
 
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
+   
 @app.route("/")
 def steam_login():
     login = SteamSignIn()
@@ -32,7 +34,60 @@ def login_process():
     return jsonify({'steam_id': '{}'.format(steam_id)})
   else:
     return 'failed login.'
+  
+@app.route("/api/ownedgames")
+def get_owned_games():
+  url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
+  data = request.values
+  if "steamid" in data.keys():
+    params = {
+      "key": os.getenv('API_KEY'),
+      "steamid": data['steamid']
+    }
+    response = requests.get(url=url, params=params)
+    if response.status_code == 200:
+      return response.json()
+    else:
+      return 'failure to get owned games. Error: HTTP {}'.format(response.status_code)
+  else:
+    return 'invalid steamid'
+  
+@app.route("/api/recentlyplayedgames")
+def get_recently_played_games():
+  url = "https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1"
+  data = request.values
+  if "steamid" in data.keys():
+    params = {
+      "key": os.getenv('API_KEY'),
+      "steamid": data['steamid']
+    }
+    response = requests.get(url=url, params=params)
+    if response.status_code == 200:
+      return response.json()
+    else: 
+      return 'failure to get recently played games. Error: HTTP {}'.format(response.status_code)
+  else:
+    return "invalid steamid"     
+  
+@app.route("/api/steamlevel")
+def get_steam_level():
+  url = "https://api.steampowered.com/IPlayerService/GetSteamLevel/v1"
+  data = request.values
+  if "steamid" in data.keys():
+    params = {
+      "key": os.getenv('API_KEY'),
+      "steamid": data['steamid']
+    }
+    response = requests.get(url=url, params=params)
+    if response.status_code == 200:
+      return response.json()
+    else: 
+      return 'failure to get steam level games. Error: HTTP {}'.format(response.status_code)
+  else:
+    return "invalid steamid"  
+
+     
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+  app.run(host='0.0.0.0', port=8000, debug=True)
