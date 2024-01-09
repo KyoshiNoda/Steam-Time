937fec571e902c0c923253d4fa82d81e515447af
Dylan Nandlall <dan6705@rit.edu>
2024-01-08 20:06:47 -0500

    created createaccount endpoint that adds a user to the database if it doesn't exists

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from database.db import db
 from database.models import User
 from dotenv import load_dotenv
 from pysteamsignin.steamsignin import SteamSignIn
+import bcrypt
 import requests
 import os
 load_dotenv()
@@ -12,12 +13,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-   
 @app.route("/")
 def steam_login():
     login = SteamSignIn()
     return login.RedirectUser(login.ConstructURL('http://localhost:8000/api/login'))
-
 
 @app.route("/api/users")
 def get_users():
@@ -35,6 +34,33 @@ def login_process():
   else:
     return 'failed login.'
   
+@app.route("/api/createaccount", methods=['POST'])
+def create_account():
+  userid=request.form['userid']
+  email=request.form['email']
+  firstname=request.form['firstname']
+  lastname=request.form['lastname']
+  apikey_hash = str(bcrypt.hashpw(request.form['apikey'].encode('utf-8'), bcrypt.gensalt()))
+  
+  if User.query.get(userid):
+    return 'account already exists'
+  elif bool(User.query.filter_by(email=email).first()):
+    return 'email is in use'
+  else:
+    user = User(
+      userid=userid,
+      email=email,
+      firstname=firstname,
+      lastname=lastname,
+      password='',
+      apikey=apikey_hash
+    )
+
+    db.session.add(user)
+    db.session.commit()
+    return Response("Account Creation Successful", status=200, mimetype='application/json')
+
+
 @app.route("/api/ownedgames")
 def get_owned_games():
   url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
